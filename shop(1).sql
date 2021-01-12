dce5878e0e821c6e76155781077286eb8b653f91
Benito <56825473+TheBenitoo@users.noreply.github.com>
2021-01-12 09:34:36 +0100

    Add files via upload

---
+++ shop(1).sql
@@ -0,0 +1,215 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 12. Jan 2021 um 09:31
+-- Server-Version: 10.4.13-MariaDB
+-- PHP-Version: 7.4.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `addresses`
+--
+
+CREATE TABLE `addresses` (
+  `AddressID` int(11) NOT NULL,
+  `Street` varchar(80) NOT NULL,
+  `StreetNumber` varchar(10) NOT NULL,
+  `Postcode` varchar(12) NOT NULL,
+  `City` varchar(85) NOT NULL,
+  `Country` varchar(60) NOT NULL,
+  `UserID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `cart`
+--
+
+CREATE TABLE `cart` (
+  `CartID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `Created` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `cartitems`
+--
+
+CREATE TABLE `cartitems` (
+  `CartItemsID` int(11) NOT NULL,
+  `ProductID` int(11) NOT NULL,
+  `CartID` int(11) NOT NULL,
+  `Quantaty` int(11) DEFAULT NULL,
+  `Created` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `payment_methods`
+--
+
+CREATE TABLE `payment_methods` (
+  `PaymentID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `CCNumber` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `product`
+--
+
+CREATE TABLE `product` (
+  `ProductID` int(11) NOT NULL,
+  `ProductName` varchar(100) DEFAULT NULL,
+  `ProductCategory` varchar(20) DEFAULT NULL,
+  `ProductDescription` varchar(100) DEFAULT NULL,
+  `ImageSource` varchar(50) DEFAULT NULL,
+  `ProductStock` int(11) DEFAULT NULL,
+  `ProductPrice` decimal(7,2) DEFAULT NULL,
+  `ProductRetail` decimal(7,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Daten für Tabelle `product`
+--
+
+INSERT INTO `product` (`ProductID`, `ProductName`, `ProductCategory`, `ProductDescription`, `ImageSource`, `ProductStock`, `ProductPrice`, `ProductRetail`) VALUES
+(1, 'Der Schrei', 'Artwork', 'Just Art', '../images/300px-The_Scream.jpg', 10, '200.00', '0.00'),
+(2, 'Mona Lisa', 'Artwork', 'Bla Bla', '../images/mona_lisa.jpg', 1, '99999.99', '0.00'),
+(3, 'Eisblumen', 'Artwork', 'Eisblumen , blau, weiß', '../images/eisblumen.jpg', 40, '299.00', '0.00');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `users`
+--
+
+CREATE TABLE `users` (
+  `UserID` int(11) NOT NULL,
+  `Username` varchar(45) DEFAULT NULL,
+  `FirstName` varchar(256) DEFAULT NULL,
+  `LastName` varchar(256) NOT NULL,
+  `PASSWORD` varchar(70) DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL,
+  `AddressID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Daten für Tabelle `users`
+--
+
+INSERT INTO `users` (`UserID`, `Username`, `FirstName`, `LastName`, `PASSWORD`, `Email`, `AddressID`) VALUES
+(1, 'USER', 'Max Mustermann', '0', '*676243218923905CF94CB52A3C9D3EB30CE8E20D', 'Max.Mustermann@gmx.de', 0);
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `addresses`
+--
+ALTER TABLE `addresses`
+  ADD PRIMARY KEY (`AddressID`) USING BTREE,
+  ADD KEY `FOREIGN` (`UserID`) USING BTREE;
+
+--
+-- Indizes für die Tabelle `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`CartID`),
+  ADD KEY `FOREIGN` (`UserID`) USING BTREE;
+
+--
+-- Indizes für die Tabelle `cartitems`
+--
+ALTER TABLE `cartitems`
+  ADD PRIMARY KEY (`CartItemsID`),
+  ADD KEY `FOREIGN` (`ProductID`,`CartID`);
+
+--
+-- Indizes für die Tabelle `payment_methods`
+--
+ALTER TABLE `payment_methods`
+  ADD PRIMARY KEY (`PaymentID`),
+  ADD KEY `FOREIGN` (`UserID`);
+
+--
+-- Indizes für die Tabelle `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`ProductID`);
+
+--
+-- Indizes für die Tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`UserID`),
+  ADD UNIQUE KEY `email` (`Email`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `addresses`
+--
+ALTER TABLE `addresses`
+  MODIFY `AddressID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `CartID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `cartitems`
+--
+ALTER TABLE `cartitems`
+  MODIFY `CartItemsID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `payment_methods`
+--
+ALTER TABLE `payment_methods`
+  MODIFY `PaymentID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `product`
+--
+ALTER TABLE `product`
+  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT für Tabelle `users`
+--
+ALTER TABLE `users`
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
